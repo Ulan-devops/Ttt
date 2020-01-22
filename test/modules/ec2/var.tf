7ed175abd52d3f70fda2baa593e5f23a94322525
ulan <umanasov24@gmail.com>
2020-01-22 17:07:19 -0600

    ss

---
+++ test/modules/ec2/var.tf
@@ -0,0 +1,15 @@
+count = "${var.ec2_count}"
+    ami     = "${var.ami_id}"
+    instance_type = "{var.instance_type}"
+    subnet_id   = "{var.subnet_id}"
+
+variable "ec2_count" {
+    default = "1"
+}
+variable "ami_id" {}
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
+variable "subnet_id" {}
+
