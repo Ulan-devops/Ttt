b91030a6afca1d36f9e7bdf9e45e4f313a2ab3d5
ulan <umanasov24@gmail.com>
2020-01-23 12:55:31 -0600

    added

--- test/new-task-deep-dive/terraform/ec2-instance.tf
+++ test/new-task-deep-dive/terraform/ec2-instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-new-instance" {
-    ami  = "ami-e689729e"
+    ami  = "ami-0dfa0289aa742513b"
     instance_type = "t2.micro"
     tags {
         Name "Terraform"
